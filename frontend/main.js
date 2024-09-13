e64fb81405d9c88841740140c6958c42add63d7f
ccbc7 <flexibleelbow@gmail.com>
2024-09-13 23:43:59 +0900

    ドム生成

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,22 @@
 import "./style.css";
 
+const onClickAdd = () => {
+  const inputElement = document.getElementById("input").value;
+  document.getElementById("input").value = "";
 
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.classList.add("list-row");
+
+  const p = document.createElement("p");
+  p.classList.add("todo-item");
+  p.innerText = inputElement;
+
+  div.appendChild(p);
+  li.appendChild(div);
+
+  document.getElementById("incompolete-list").appendChild(li);
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
